5648798a5276ab6b0946957219da82ef69b439c0
santhanajonesj <155952919+santhanajonesj@users.noreply.github.com>
2024-06-21 19:11:34 +0530

    streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -59,21 +59,7 @@ if len(channel_id)>0:
         video2 = get_Video_Details(youtube,channel_id)
         channel_data = get_channel_info(youtube, channel_id)
         
-        # VIEW PAGE
-  
-    st.write("## :orange[Select any question to get Insights]")
-    questions = st.selectbox('Questions',
-    ['Click the question that you would like to query',
-    '1. What are the names of all the videos and their corresponding channels?',
-    '2. Which channels have the most number of videos, and how many videos do they have?',
-    '3. What are the top 10 most viewed videos and their respective channels?',
-    '4. How many comments were made on each video, and what are their corresponding video names?',
-    '5. Which videos have the highest number of likes, and what are their corresponding channel names?',
-    '6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?',
-    '7. What is the total number of views for each channel, and what are their corresponding channel names?',
-    '8. What are the names of all the channels that have published videos in the year 2022?',
-    '9. What is the average duration of all videos in each channel, and what are their corresponding channel names?',
-    '10. Which videos have the highest number of comments, and what are their corresponding channel names?'])
+       
        
        
     if questions == '1. What are the names of all the videos and their corresponding channels?':
